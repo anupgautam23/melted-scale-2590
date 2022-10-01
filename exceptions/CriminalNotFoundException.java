f52d30c707a7e68ce5eb5c5b6248660a4881d5c4
anupgautam23 <anupgautam97@gmail.com>
2022-10-01 19:43:03 +0530

    day 4 jdbc and dao implementation files

---
+++ exceptions/CriminalNotFoundException.java
@@ -0,0 +1,12 @@
+package com.masai.exceptions;
+
+public class CriminalNotFoundException extends Exception{
+	
+	public CriminalNotFoundException() {
+		
+	}
+	public CriminalNotFoundException(String message) {
+		super(message);
+	}
+
+}
